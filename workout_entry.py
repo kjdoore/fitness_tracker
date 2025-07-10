b207a1e762d49b993b8ada83aba346e5c8c10130
kjdoore <kjdoore@gmail.com>
2025-07-09 21:39:39 -0500

    Add superset and RPE entry

--- workout_entry.py
+++ workout_entry.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import duckdb
 import datetime
+import numpy as np
 
 st.title("Workout Entry Sheet")
 st.set_page_config(page_icon="🏋️")
@@ -16,7 +17,9 @@ conn.execute("""
         exercise TEXT,
         weight INTEGER,
         reps INTEGER,
-        set INTEGER
+        set INTEGER,
+        rpe INTEGER,
+        super_set INTEGER,
     )
 """)
 conn.execute("""
@@ -62,13 +65,32 @@ else:
     st.selectbox("Select exercise muscle group", ["No Exercises in System"], index=0, disabled=True)
     st.selectbox("Select exercise", ["No Exercises in System"], index=0, disabled=True)
     exercise = None
-# Do weight steps in 5 lb intervals. Can still type any integer,
-# but make the +/- buttons go in increments of 5.
-weight = st.number_input("Weight (lbs)", min_value=0, step=5)
-reps = st.number_input("Reps", min_value=1)
+
 set_num = st.number_input("Set", min_value=1)
+column2 = st.columns(2)
+with column2[0]:
+    super_set = st.checkbox("Super Set")
+with column2[1]:
+    # We only want a super set value if it is a super set
+    if super_set:
+        super_set_num = st.number_input("Super Set", min_value=1)
+    else:
+        super_set_num = None
+
+column3 = st.columns(3)
+with column3[0]:
+    # Do weight steps in 5 lb intervals. Can still type any integer,
+    # but make the +/- buttons go in increments of 5.
+    weight = st.number_input("Weight (lbs)", min_value=0, step=5)
+with column3[1]:
+    reps = st.number_input("Reps", min_value=1)
+with column3[2]:
+    rpe = st.number_input("RPE", min_value=1, max_value=10)
+
 
 # Initialize session state
+if "needs_confirmation" not in st.session_state:
+    st.session_state.needs_confirmation = False
 if "submitted" not in st.session_state:
     st.session_state.submitted = False
 
@@ -84,34 +106,37 @@ with st.form("entry_form", border=False):
             'exercise': exercise,
             'weight': weight,
             'reps': reps,
-            'set_num': set_num
+            'set_num': set_num,
+            'rpe': rpe,
+            'super_set_num': super_set_num,
         }
 
 # Run logic after form submission
 if st.session_state.get("submitted"):
     data = st.session_state.form_data
-    # Check if entry exists in table
+    # Check if workout and set entry exists in table
     exists = conn.execute(f"""
         SELECT 1 FROM fitness_data
         WHERE username = '{data['username']}'
         AND date = DATE '{data['date']}'
         AND exercise = '{data['exercise']}'
-        AND weight = {data['weight']}
-        AND reps = {data['reps']}
         AND set = {data['set_num']}
         """
     ).fetchone()
 
+
     if not exists and data['username'] and data['exercise']:
         conn.execute(f"""
-            INSERT INTO fitness_data (username, date, exercise, weight, reps, set)
-            VALUES (?, ?, ?, ?, ?, ?)
+            INSERT INTO fitness_data (username, date, exercise, weight, reps, set, rpe, super_set)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
             """,
-            [username, date, exercise, weight, reps, set_num]
+            [data['username'], data['date'], data['exercise'],
+             data['weight'], data['reps'], data['set_num'],
+             data['rpe'], data['super_set_num']]
         )
         st.success("Entry added!")
     elif exists:
-        st.warning("Entry already exists.")
+        st.session_state.needs_confirmation = True
     elif not data['username']:
         st.warning("Select a User.")
     elif not data['exercise']:
@@ -119,6 +144,30 @@ if st.session_state.get("submitted"):
 
     # Reset flag so it doesn't rerun on every refresh
     st.session_state.submitted = False
+
+if st.session_state.needs_confirmation:
+    st.warning("Set entry already exists.")
+    # Show a confirmation button
+    if st.button("Confirm Override"):
+        data = st.session_state.form_data
+        conn.execute(f"""
+            DELETE FROM fitness_data
+            WHERE username = '{data['username']}'
+            AND date = DATE '{data['date']}'
+            AND exercise = '{data['exercise']}'
+            AND set = {data['set_num']}
+        """)
+        conn.execute(f"""
+            INSERT INTO fitness_data (username, date, exercise, weight, reps, set, rpe, super_set)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            [data['username'], data['date'], data['exercise'],
+             data['weight'], data['reps'], data['set_num'],
+             data['rpe'], data['super_set_num']]
+        )
+        st.success("Entry overridden successfully.")
+
+        st.session_state.needs_confirmation = False
 
 # View most recent 20 entries
 if st.checkbox("Show 20 most recent entries"):
